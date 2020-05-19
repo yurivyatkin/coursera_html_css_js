d7e2ff95b92054e4ed52b39cf67cd0dc505e19dc
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-19 22:20:54 +1200

    STEP 7

--- assignment4/SpeakGoodBye.js
+++ assignment4/SpeakGoodBye.js
@@ -8,7 +8,7 @@
   // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
   // the "speak" method and which you will expose to the global context
   // See Lecture 52, part 1
-  // var byeSpeaker =
+  var byeSpeaker = {};
 
   // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
   var speakWord = 'Good Bye';
