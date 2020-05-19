450517261e368692e6b9a1195a0cbe0fa4b030d4
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-19 22:16:51 +1200

    STEP 5

--- assignment4/SpeakHello.js
+++ assignment4/SpeakHello.js
@@ -1,4 +1,4 @@
-(function () {
+(function (window) {
   // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
   // See Lecture 52, part 2
 
@@ -21,5 +21,5 @@
   // 'helloSpeaker' on the global scope as well.
   // See Lecture 52, part 2
   // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-  // xxxx.xxxx = helloSpeaker;
-})();
+  window.helloSpeaker = helloSpeaker;
+})(window);
