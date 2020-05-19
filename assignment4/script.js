8572ce3c2a2e098c2c7158f84f4a46cda2a584dd
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-19 22:28:53 +1200

    STEP 11

--- assignment4/script.js
+++ assignment4/script.js
@@ -52,7 +52,7 @@
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-    // var firstLetter =
+    var firstLetter = names[idx].charAt(0).toLowerCase();
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
